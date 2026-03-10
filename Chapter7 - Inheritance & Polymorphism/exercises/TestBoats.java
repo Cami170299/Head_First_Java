735133bd98f241fa75ecb923aab0b4c7bf105d5a
Cami <86859620+Cami170299@users.noreply.github.com>
2026-03-10 17:06:11 -0400

    boat exercise

--- Chapter7 - Inheritance & Polymorphism/exercises/TestBoats.java
+++ Chapter7 - Inheritance & Polymorphism/exercises/TestBoats.java
@@ -1,6 +1,41 @@
+class Boat {
+  private int lenght;
 
-class TestBoats {
+  public void setLenght(int len) {
+    lenght = len;
+  }
+
+  public int getLenght(int len) {
+    return lenght;
+  }
+
+  public void move() {
+    System.out.print("Drift");
+  }
+}
+
+class Sailboat extends Boat {
+  @Override
+  public void move() {
+    System.out.print("Hoist Sail !");
+  }
+}
+
+class Rowboat extends Boat {
+  public void rowTheBoat() {
+    System.out.print("Stroke Natasha !");
+  }
+}
+
+public class TestBoats {
   public static void main(String[] args) {
+    Boat b1 = new Boat();
+    Sailboat b2 = new Sailboat();
+    Rowboat b3 = new Rowboat();
 
+    b2.setLenght(32);
+    b1.move();
+    b3.move();
+    b2.move();
   }
 }
