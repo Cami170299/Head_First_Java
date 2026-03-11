ba4831b361dfea72ea5bc12998924080501b5e5e
Cami <86859620+Cami170299@users.noreply.github.com>
2026-03-10 21:29:09 -0400

    chapter8

---
+++ Chapter8 - Interfaces & Abstract Classes/examples/TheObjectClass.java
@@ -0,0 +1,9 @@
+public class TheObjectClass {
+  public static void main(String[] args) {
+    AnyObject o1 = new AnyObject();
+    System.out.println(o1.toString());
+  }
+}
+
+class AnyObject {
+}
